055add1e2700882b5ae054a5c35a16e0293a7628
Aleksandra <elinda100@gmail.com>
2023-06-07 20:29:47 +0200

    finished first animation on main page and added heading with students opinions

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -4,30 +4,60 @@ import Grid from "@mui/material/Grid"; // Grid version 1
 
 import { Box, Typography } from "@mui/material";
 import { grey } from "@mui/material/colors";
+import Paper from "@mui/material/Paper";
+import {
+  createTheme,
+  responsiveFontSizes,
+  ThemeProvider,
+} from "@mui/material/styles";
+
+let theme = createTheme();
+theme = responsiveFontSizes(theme);
 
 export default function Home() {
   return (
-    <Box className="flex justify-around" sx={{ alignItems: "center" }}>
-      <Grid
-        container
-        spacing={4}
-        direction="row"
-        justifyContent="center"
-        alignItems="center"
-        marginX={5}
+    <ThemeProvider theme={theme}>
+      <Box
+        className="flex justify-around flex-col"
+        sx={{ alignItems: "center" }}
       >
-        <Grid item xs={5}>
-          <Lottie animationData={animationData} loop={true} />
+        <Grid
+          container
+          spacing={4}
+          direction="row"
+          justifyContent="center"
+          alignItems="center"
+          marginX={5}
+        >
+          <Grid item xs={10} sm={7} md={5} lg={4}>
+            <Lottie animationData={animationData} loop={true} />
+          </Grid>
+          <Grid item xs={12} sm={5} md={5} lg={5} className="mr-1">
+            <Typography
+              variant="h3"
+              color={grey[900]}
+              className="font-light text-center"
+            >
+              Ucz się{" "}
+              <span className="font-medium  text-blue-500 text-center">
+                szybciej
+              </span>
+              .
+            </Typography>
+            <Typography
+              variant="h3"
+              color={grey[900]}
+              className="font-light text-center"
+            >
+              Zapamiętuj{" "}
+              <span className="font-medium text-blue-500">więcej</span>.
+            </Typography>
+          </Grid>
         </Grid>
-        <Grid item xs={6} className="">
-          <Typography variant="h4" color={grey[900]}>
-            Ucz się szybciej.
-          </Typography>
-          <Typography variant="h4" color={grey[900]}>
-            Zapamiętuj więcej.
-          </Typography>
-        </Grid>
-      </Grid>
-    </Box>
+        <Paper>
+          <Typography variant="h4">Co studenci mówią o Flashwise?</Typography>
+        </Paper>
+      </Box>
+    </ThemeProvider>
   );
 }
