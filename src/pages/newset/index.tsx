2f43a4727d3ece031e6bf82c8f243086fe3f1a8d
Aleksandra <elinda100@gmail.com>
2023-06-11 18:39:37 +0200

    added new set page

--- src/pages/newset/index.tsx
+++ src/pages/newset/index.tsx
@@ -1,10 +1,120 @@
+import { useState } from "react";
+import { Typography, IconButton, Button } from "@mui/material";
 import Breadcrumb from "../../components/breadcrumb";
+import TextField from "@mui/material/TextField";
+import AddIcon from "@mui/icons-material/Add";
+import DeleteIcon from "@mui/icons-material/Delete";
+
+interface Card {
+  term: string;
+  definition: string;
+}
 
 export default function NewSet() {
+  const [cards, setCards] = useState<Card[]>([{ term: "", definition: "" }]);
+
+  const handleAddCard = () => {
+    setCards([...cards, { term: "", definition: "" }]);
+  };
+
+  const handleTermChange = (
+    event: React.ChangeEvent<HTMLInputElement>,
+    index: number
+  ) => {
+    const newCards = [...cards];
+    newCards[index].term = event.target.value;
+    setCards(newCards);
+  };
+
+  const handleDefinitionChange = (
+    event: React.ChangeEvent<HTMLInputElement>,
+    index: number
+  ) => {
+    let updatedList = [...cards];
+    updatedList[index].definition = event.target.value;
+    setCards(updatedList);
+  };
+
+  const handleDeleteCardClick = (index: number) => {
+    let updatedList = [...cards];
+    updatedList.splice(index, 1);
+    setCards(updatedList);
+  };
+
   return (
     <>
       <div>
-        <Breadcrumb name="Nowy set" />
+        <Breadcrumb name="Nowy zestaw" />
+      </div>
+      <div>
+        <Typography variant="h4" className="px-5 text-gray-900">
+          Stwórz nowy zestaw
+        </Typography>
+
+        {/* Nazwa zestawu */}
+        <div>
+          <TextField
+            className="w-1/2 mx-5 my-3"
+            id="set-name"
+            label="Nazwa zestawu"
+            variant="standard"
+          />
+          {/*Przycisk Stwórz zestaw*/}
+          <Button
+            variant="contained"
+            className="bg-blue-500 my-5 ml-10"
+            size="large"
+          >
+            Stwórz zestaw
+          </Button>
+        </div>
+
+        {/* Karty fiszek */}
+        {cards.map((card: Card, index: number) => (
+          <div
+            key={index}
+            className="flex justify-between items-center my-3 mx-5"
+          >
+            <TextField
+              className="flex-1 mr-2"
+              id={`term-${index}`}
+              label="Pojęcie"
+              variant="standard"
+              value={card.term}
+              onChange={(event: React.ChangeEvent<HTMLInputElement>) => {
+                handleTermChange(event, index);
+              }}
+            />
+
+            <TextField
+              className="flex-1 ml=2"
+              id={`definition-${index}`}
+              label="Definicja"
+              variant="standard"
+              value={card.definition}
+              onChange={(event: React.ChangeEvent<HTMLInputElement>) => {
+                handleDefinitionChange(event, index);
+              }}
+            />
+
+            {/* Przycisk usuwanie karty */}
+
+            <IconButton onClick={() => handleDeleteCardClick(index)}>
+              <DeleteIcon />
+            </IconButton>
+          </div>
+        ))}
+
+        {/* Przycisk dodawania kart */}
+        <div className="flex justify-center mt=5">
+          <IconButton
+            onClick={() => {
+              handleAddCard();
+            }}
+          >
+            <AddIcon />
+          </IconButton>
+        </div>
       </div>
     </>
   );
