c50ebe8303591192105a1c0071612ff3157cf854
Aleksandra <elinda100@gmail.com>
2023-06-12 16:22:53 +0200

    improved new set page and added my sets page

--- src/pages/mysets/index.tsx
+++ src/pages/mysets/index.tsx
@@ -1,10 +1,22 @@
 import Breadcrumb from "../../components/breadcrumb";
+import Link from "next/link";
 
 export default function MySets() {
+  const sets = ["unit 1", "nazwy krajów", "czasowniki nieregularne"];
   return (
     <>
       <div>
         <Breadcrumb name="Moje zestawy" />
+      </div>
+      <div className="sm:grid sm:grid-cols-3 lg:grid-cols-4 xl:grid-cols-5">
+        {sets.map((value, valueIndex) => (
+          <div
+            key={valueIndex}
+            className="p-7 m-6 bg-blue-500 text-white text-center shadow-xl rounded-md flex items-center justify-center"
+          >
+            <Link href="/">{value}</Link>
+          </div>
+        ))}
       </div>
     </>
   );
--- src/pages/newset/index.tsx
+++ src/pages/newset/index.tsx
@@ -4,6 +4,7 @@ import Breadcrumb from "../../components/breadcrumb";
 import TextField from "@mui/material/TextField";
 import AddIcon from "@mui/icons-material/Add";
 import DeleteIcon from "@mui/icons-material/Delete";
+import { Box } from "@mui/material";
 
 interface Card {
   term: string;
@@ -37,6 +38,7 @@ export default function NewSet() {
 
   const handleDeleteCardClick = (index: number) => {
     let updatedList = [...cards];
+    if (updatedList.length === 1) return;
     updatedList.splice(index, 1);
     setCards(updatedList);
   };
@@ -46,13 +48,13 @@ export default function NewSet() {
       <div>
         <Breadcrumb name="Nowy zestaw" />
       </div>
-      <div>
+      <div className="mx-8 my-3">
         <Typography variant="h4" className="px-5 text-gray-900">
           Stwórz nowy zestaw
         </Typography>
 
         {/* Nazwa zestawu */}
-        <div>
+        <div className="flex justify-between">
           <TextField
             className="w-1/2 mx-5 my-3"
             id="set-name"
@@ -62,59 +64,69 @@ export default function NewSet() {
           {/*Przycisk Stwórz zestaw*/}
           <Button
             variant="contained"
-            className="bg-blue-500 my-5 ml-10"
+            className="bg-blue-500 my-5 mr-10"
             size="large"
           >
             Stwórz zestaw
           </Button>
         </div>
+        <Box
+          sx={{
+            mb: 2,
+            display: "flex",
+            flexDirection: "column",
+            height: "70vh",
+            overflow: "hidden",
+            overflowY: "scroll",
+          }}
+        >
+          {/* Karty fiszek */}
+          {cards.map((card: Card, index: number) => (
+            <div
+              key={index}
+              className="flex justify-between items-center my-3 mx-5"
+            >
+              <TextField
+                className="flex-1 mr-2"
+                id={`term-${index}`}
+                label="Pojęcie"
+                variant="standard"
+                value={card.term}
+                onChange={(event: React.ChangeEvent<HTMLInputElement>) => {
+                  handleTermChange(event, index);
+                }}
+              />
 
-        {/* Karty fiszek */}
-        {cards.map((card: Card, index: number) => (
-          <div
-            key={index}
-            className="flex justify-between items-center my-3 mx-5"
-          >
-            <TextField
-              className="flex-1 mr-2"
-              id={`term-${index}`}
-              label="Pojęcie"
-              variant="standard"
-              value={card.term}
-              onChange={(event: React.ChangeEvent<HTMLInputElement>) => {
-                handleTermChange(event, index);
+              <TextField
+                className="flex-1 ml=2"
+                id={`definition-${index}`}
+                label="Definicja"
+                variant="standard"
+                value={card.definition}
+                onChange={(event: React.ChangeEvent<HTMLInputElement>) => {
+                  handleDefinitionChange(event, index);
+                }}
+              />
+
+              {/* Przycisk usuwanie karty */}
+
+              <IconButton onClick={() => handleDeleteCardClick(index)}>
+                <DeleteIcon />
+              </IconButton>
+            </div>
+          ))}
+
+          {/* Przycisk dodawania kart */}
+          <div className="flex justify-center mt-5 mr-10">
+            <IconButton
+              onClick={() => {
+                handleAddCard();
               }}
-            />
-
-            <TextField
-              className="flex-1 ml=2"
-              id={`definition-${index}`}
-              label="Definicja"
-              variant="standard"
-              value={card.definition}
-              onChange={(event: React.ChangeEvent<HTMLInputElement>) => {
-                handleDefinitionChange(event, index);
-              }}
-            />
-
-            {/* Przycisk usuwanie karty */}
-
-            <IconButton onClick={() => handleDeleteCardClick(index)}>
-              <DeleteIcon />
+            >
+              <AddIcon />
             </IconButton>
           </div>
-        ))}
-
-        {/* Przycisk dodawania kart */}
-        <div className="flex justify-center mt=5">
-          <IconButton
-            onClick={() => {
-              handleAddCard();
-            }}
-          >
-            <AddIcon />
-          </IconButton>
-        </div>
+        </Box>
       </div>
     </>
   );
