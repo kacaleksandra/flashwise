dd0dba179eadbddeb622bae86ce04acbbfb450ed
Aleksandra <elinda100@gmail.com>
2023-06-17 17:36:13 +0200

    added table with flashcards

--- src/pages/set/[id].tsx
+++ src/pages/set/[id].tsx
@@ -1,17 +1,57 @@
-import { useRouter } from "next/router";
 import { useEffect, useState } from "react";
 import { useTokenStore } from "@/store/useTokenStore";
-import CircularProgress from "@mui/material/CircularProgress";
 import IFlashcard from "@/interfaces/Flashcard";
+import Table from "@mui/material/Table";
+import TableHead from "@mui/material/TableHead";
+import TableBody from "@mui/material/TableBody";
+import TableCell from "@mui/material/TableCell";
+import TableContainer from "@mui/material/TableContainer";
+import TableFooter from "@mui/material/TableFooter";
+import TablePagination from "@mui/material/TablePagination";
+import TableRow from "@mui/material/TableRow";
+import Paper from "@mui/material/Paper";
+import * as st from "@/pages/set/StyledTable";
+import { Typography } from "@mui/material";
+import Button from "@mui/material/Button";
+import { useMySetsStore } from "@/store/useMySetsStore";
+import { useRouter } from "next/router";
+import ISet from "@/interfaces/Set";
 
 export default function Set() {
   const token = useTokenStore((state) => state.token);
+  const mySets = useMySetsStore((state) => state.sets);
   const router = useRouter();
 
-  async function fetchFlashcards(nameOnly: string): Promise<void> {
+  async function getSet(): Promise<ISet> {
+    const id = window.location.pathname.substring(
+      window.location.pathname.lastIndexOf("/") + 1
+    );
+
+    const response = await fetch(
+      `http://vbujdewvbj.cfolks.pl/api/sets?flashcard_set_id=` + id,
+      {
+        method: "GET",
+        headers: {
+          Authorization: `Token ${token}`,
+        },
+      }
+    );
+    const set = (await response.json()) ?? [];
+    return set[0];
+  }
+
+  async function fetchFlashcards(): Promise<void> {
     try {
+      const set = await getSet();
+      if (set === undefined) {
+        await router.push("/categories");
+        return;
+      }
+      setSetName(set.name);
+
       const response = await fetch(
-        `http://vbujdewvbj.cfolks.pl/api/flashcards?flashcard_set=` + nameOnly,
+        `http://vbujdewvbj.cfolks.pl/api/flashcards?flashcard_set=` + set.name,
+
         {
           method: "GET",
           headers: {
@@ -19,39 +59,124 @@ export default function Set() {
           },
         }
       );
-      console.log(response);
       const data = await response.json();
 
-      await console.log(data);
-      setFlashcards(
-        data.map(({ id, front, back }: IFlashcard) => ({ id, front, back }))
-      );
+      setFlashcards(data);
     } catch (error) {
       console.error(error);
     }
   }
-
   const [flashcards, setFlashcards] = useState<IFlashcard[]>([]);
+  const [setName, setSetName] = useState<string>("");
 
   useEffect(() => {
-    //fetch flashcards
-    const id = window.location.pathname.substring(
-      window.location.pathname.lastIndexOf("/") + 1
-    );
-    const nameOnly = id.substring(id.indexOf("-") + 1);
-
-    fetchFlashcards(nameOnly);
+    fetchFlashcards();
   }, []);
+
+  // tworzenie tabeli
+
+  const headers = ["Przód fiszki", "Tył fiszki"];
+
+  function createData(front: string, back: string) {
+    return { front, back };
+  }
+
+  const rows = flashcards.map((flashcard) =>
+    createData(flashcard.front, flashcard.back)
+  );
+
+  const [page, setPage] = useState(0);
+  const [rowsPerPage, setRowsPerPage] = useState(5);
+
+  const emptyRows =
+    page > 0 ? Math.max(0, (1 + page) * rowsPerPage - rows.length) : 0;
+
+  const handleChangePage = (
+    event: React.MouseEvent<HTMLButtonElement> | null,
+    newPage: number
+  ) => {
+    setPage(newPage);
+  };
+
+  const handleChangeRowsPerPage = (
+    event: React.ChangeEvent<HTMLInputElement | HTMLTextAreaElement>
+  ) => {
+    setRowsPerPage(parseInt(event.target.value, 10));
+    setPage(0);
+  };
 
   return (
     <>
-      <h1>test </h1>
-      {flashcards.map((flashcard) => (
-        <div key={flashcard.id}>
-          <h1>{flashcard.front}</h1>
-          <h1>{flashcard.back}</h1>
+      <div className="px-5 py-3 mt-3 w-full flex justify-around">
+        <div>
+          <Typography variant="h4" className="text-blue-500">
+            {setName}
+          </Typography>
         </div>
-      ))}
+        <Button variant="contained" className="bg-blue-500 text-white">
+          Przejdź do fiszek
+        </Button>
+        <Button variant="contained" className="bg-blue-500 text-white">
+          Wygeneruj quiz
+        </Button>
+
+        <Button variant="contained" className="bg-blue-500 text-white">
+          Edytuj
+        </Button>
+      </div>
+      <div className="flex justify-center my-5">
+        <TableContainer component={Paper} className="w-11/12">
+          <Table sx={{ minWidth: 500 }} aria-label="custom pagination table">
+            <TableHead>
+              <TableRow className="bg-blue-50">
+                <TableCell align="center">Przód fiszki</TableCell>
+                <TableCell align="center">Tył fiszki</TableCell>
+              </TableRow>
+            </TableHead>
+            <TableBody>
+              {(rowsPerPage > 0
+                ? rows.slice(
+                    page * rowsPerPage,
+                    page * rowsPerPage + rowsPerPage
+                  )
+                : rows
+              ).map((row) => (
+                <TableRow key={row.front}>
+                  <TableCell scope="row" align="center">
+                    {row.front}
+                  </TableCell>
+                  <TableCell align="center">{row.back}</TableCell>
+                </TableRow>
+              ))}
+              {emptyRows > 0 && (
+                <TableRow style={{ height: 53 * emptyRows }}>
+                  <TableCell colSpan={6} />
+                </TableRow>
+              )}
+            </TableBody>
+            <TableFooter>
+              <TableRow>
+                <TablePagination
+                  rowsPerPageOptions={[5, 10, 25, { label: "All", value: -1 }]}
+                  colSpan={3}
+                  count={rows.length}
+                  rowsPerPage={rowsPerPage}
+                  page={page}
+                  SelectProps={{
+                    inputProps: {
+                      "aria-label": "Wierszy na stronę",
+                    },
+                    native: true,
+                  }}
+                  onPageChange={handleChangePage}
+                  onRowsPerPageChange={handleChangeRowsPerPage}
+                  ActionsComponent={st.TablePaginationActions}
+                />
+              </TableRow>
+            </TableFooter>
+          </Table>
+        </TableContainer>
+      </div>
     </>
   );
 }
